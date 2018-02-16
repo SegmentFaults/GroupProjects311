174b5a457ce1366571af1c04b9c43e2afd9f9692
Michael Gentile <gentilm5@miamioh.edu>
2018-02-16 16:26:34 -0500

    I broke this real bad
    made everything into its own entity

--- MVCTicTacToe.py
+++ MVCTicTacToe.py
@@ -1,29 +1,29 @@
 from Tkinter import *
 from enum import Enum
 from array import array
-#TODO: Break controller and model into two from the "Logic class"
-#this is the controller
-class Logic(object):
+
+class Model():
     player=True
     def __init__(self):
-        # it's going to be a lot easier just to not use a double sided
         self.game_board = array('I', [1, 1, 1, 1, 1, 1, 1, 1, 1])
         self.game_state = 4
-    #model
+        
     def get_game_board(self):
         return self.game_board
-    #controller
-    def change_state(self, board_space):
-        if self.player and self.game_board[board_space]==1:
-            self.game_board[board_space]=2
-            self.player=not self.player
-        elif self.game_board[board_space]==1:
-            self.game_board[board_space]=3
-            self.player=not self.player
-        winStatus = self.__check_win(self.player)
-        if not winStatus == 4:
-            self.game_state=winStatus
+    
+    def __check_cat(self):
+        for x in range(9):
+            if not self.game_board[x]==2 or not self.game_board[x]==3:
+                return False
+
+        return True
     #model
+    def __change_win(self, player):
+        if player:
+            self.game_state=1
+            #verify these constants later
+        else:
+            self.game_state=2
     def check_end_game(self):
         ##will return 1 for x win, 2 for y win and 3 for cat. Otherwise, it will return 4 for continue
         #checking for x win
@@ -57,54 +57,32 @@ class Logic(object):
             print "works 3"
             return True
         return False
-    #model
-    def __check_cat(self):
-        for x in range(9):
-            if not self.game_board[x]==2 or not self.game_board[x]==3:
-                return False
-
-        return True
-    def __change_win(self, player):
-        if player:
-            self.game_state=1
-            #verify these constants later
-        else:
-            self.game_state=2
-#this is the view
-class GUIView(Frame, Logic):
-    def __init__(self, master):
-        Frame.__init__(self, master)
-        self.grid()
-        self.logic = Logic()
-        self.create_widgets()
-    #move to controller
+#controller mediates between the model and the view
+class Controller(object):
+    #translates user action into changes in the model
+    def __init__(self, model):
+        self.model = model
+        # it's going to be a lot easier just to not use a double sided
+        print "Filler"
+    def change_state(self, board_space):
+        if self.player and self.game_board[board_space]==1:
+            self.game_board[board_space]=2
+            self.player=not self.player
+        elif self.game_board[board_space]==1:
+            self.game_board[board_space]=3
+            self.player=not self.player
+        winStatus = self.__check_win(self.player)
+        if not winStatus == 4:
+            self.game_state=winStatus
+    
     def on_click(self, board_space):
-        self.logic.change_state(board_space)
+        self.controller.change_state(board_space)
         self.create_widgets()
-    #move to controller
+        
     def switch_ui(self, currentLogic):
         #TODO create method that swaps them
         print "Filler"
-    
-    def create_widgets(self):
-        vertical_line=0
-        for rowSpace in range(9):
-                button_text = self.logic.get_game_board()[rowSpace]
-                #get the text from the board and make sure that you convert it for the GUI
-                button_text = self.determine_button_text(button_text)
-                button = Button(self, text=button_text, height=10, width=10, command=lambda x=rowSpace: self.on_click(x))
-                button.grid(row = rowSpace/3, column = vertical_line)
-                vertical_line+=1
-                if vertical_line==3:
-                    vertical_line=0
-        
-        game_state_label = Label(self)
-        game_state_label.config(text = self.determine_label_text(self.logic.game_state))
-        game_state_label.grid(row = 4, column = 1)
-        switch_interface_button = Button(self, text="Switch UI", width =10, height =10, command=lambda logic_instance=self.logic: self.switch_ui(logic_instance))
-        switch_interface_button.grid(row = 4, column = 2)
 
-    #move to controller
     def determine_button_text(self, game_board_integer):
         if game_board_integer == 2:
             return 'X'
@@ -114,20 +92,51 @@ class GUIView(Frame, Logic):
             return '-'
     #move to controller
     def determine_label_text(self, game_state):
-        if self.logic.game_state==1:
+        if self.controller.game_state==1:
             return "X Wins"
-        elif self.logic.game_state==2:
+        elif self.controller.game_state==2:
             return "Y Wins"
-        elif self.logic.game_state==3:
+        elif self.controller.game_state==3:
             return "Cat"
         else:
             return ""
+
+#this is the view
+class GUIView(Frame):
+    #View
+    def __init__(self, master, controller):
+        Frame.__init__(self, master)
+        self.grid()
+        self.model = Model()
+        self.controller = controller(self.model)
+        self.create_widgets()
+    def create_widgets(self):
+        vertical_line=0
+        for rowSpace in range(9):
+                button_text = self.model.get_game_board()[rowSpace]
+                #get the text from the board and make sure that you convert it for the GUI
+                button_text = self.controller.determine_button_text(button_text)
+                button = Button(self, text=button_text, height=10, width=10, command=lambda x=rowSpace: controller.on_click(x))
+                button.grid(row = rowSpace/3, column = vertical_line)
+                vertical_line+=1
+                if vertical_line==3:
+                    vertical_line=0
         
-#TODO: Implement the text based logic.
-class TextView(Logic):
-    print "Filler"
+        game_state_label = Label(self)
+        game_state_label.config(text = self.model.determine_label_text(self.model.game_state))
+        game_state_label.grid(row = 4, column = 1)
+        switch_interface_button = Button(self, text="Switch UI", width =10, height =10, command=lambda model_instance=self.model: self.switch_ui(model_instance))
+        switch_interface_button.grid(row = 4, column = 2)
+
+   
+#TODO: Implement the text based controller.
+class TextView(Controller):
+    print "We need to seperate the model and the controller first"
+
+
 mainWindow = Tk()
-obj = GUIView(mainWindow)
+m = Model()
+obj = GUIView(mainWindow, m)
 mainWindow.mainloop()
 
 
