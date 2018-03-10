ed9685a9ff4ca62ab4b0a915274f678ff487ee96
Schmidty15 <cyklussoftware@gmail.com>
2018-03-10 16:10:29 -0500

    added configuration

---
+++ PrimeFactors/ConfigureDispatcher.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+javac Dispatcher.javac
+
+java Dispatcher 3131 172.17.31.234 172.17.31.141 172.17.31.162 172.17.31.152 3131
+
---
+++ PrimeFactors/ConfigureWorker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+javac FindPrimes.java
+javac WorkServer.java
+
+java WorkServer 3131
